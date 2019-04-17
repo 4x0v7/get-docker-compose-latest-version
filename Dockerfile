03ff5cbcf71fc2f686b1864744a804c969596ef1
4x0v7 <g4x0v7@gmail.com>
2019-04-17 13:53:21 +1200

    Use jq

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
-FROM alpine:3.8
+FROM alpine:3.9
 LABEL maintainer="https://github.com/4x0v7"
 RUN apk --no-cache add \
-    curl \
-    grep
-COPY get-docker-compose-latest-version.sh /usr/local/bin/
-ENTRYPOINT [ "get-docker-compose-latest-version.sh" ]
+    jq==1.6-r0
+ENTRYPOINT [ "/bin/sh", "-c", "wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq --raw-output .tag_name" ]
--- get-docker-compose-latest-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DOCKER_COMPOSE_VERSION=$(curl \
-                         --silent \
-                         --show-error \
-                         --dump-header - \
-                        https://github.com/docker/compose/releases/latest -o /dev/null | \
-                        grep -Pzo '(?<=Location: https:\/\/github\.com\/docker\/compose\/releases\/tag\/)(.+)')
-echo "$DOCKER_COMPOSE_VERSION"
